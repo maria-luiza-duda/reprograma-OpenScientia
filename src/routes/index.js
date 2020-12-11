344a79d85f936ea843521bd259657477517b1901
Malu23 <malududa54@gmail.com>
2020-12-10 23:23:29 -0300

    Inserção index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const controller = require('../controllers/translateController.js')
 
 router.get("/", function (req, res) {
     res.status(200).send({
